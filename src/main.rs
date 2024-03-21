684292be71c0c169b914c648322c6a7c1e45de25
Liam Tietjens <l.tietjens@hotmail.com>
2024-03-21 19:45:36 +1300

    created a struct for type to contain menu items

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+use dit_as_91896;
+
+fn main() {
+    let menu_items = dit_as_91896::Items::new(vec![String::from("wow")], vec![], vec![]);
+    println!("{menu_items}");
+    println!("Hello, world!");
+}
