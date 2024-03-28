9d30004a293dcd96dde94268421aee4b8804abff
Liam Tietjens <l.tietjens@hotmail.com>
2024-03-28 19:39:39 +1300

    Added hashmaps to hold the Item name and Item price.
    Now the menu can hold the price for each Item not just the name.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
+use map_macro::hash_map;
+
 use dit_as_91896::menu;
 
 fn main() {
-    let menu_items = menu::Items::new(vec![String::from("wow")], vec![], vec![]);
-    println!("{menu_items}");
+    // let menu_items = menu::Items::new(vec![String::from("wow")], vec![], vec![]);
+    // println!("{menu_items}");
     println!("Hello, world!\n\n");
 
     let items: menu::Items = menu::Items {
-        food: vec![String::from("0")],
-        drinks: vec![String::from("1")],
-        sides: vec![String::from("2")],
+        food: hash_map! {
+            String::from("0") => 0.0,
+        },
+        drinks: hash_map! {
+            String::from("1") => 1.0,
+        },
+        sides: hash_map! {
+            String::from("2") => 2.0,
+        },
     };
-    println!("{items}")
+    println!(r#"{items}"#)
 }
--- src/menu/mod.rs
+++ src/menu/mod.rs
@@ -1,10 +1,10 @@
 use core::fmt;
-use std::fmt::Display;
+use std::{collections::HashMap, fmt::Display};
 
 pub struct Items {
-    pub food: Vec<String>,
-    pub drinks: Vec<String>,
-    pub sides: Vec<String>,
+    pub food: HashMap<String, f64>,
+    pub drinks: HashMap<String, f64>,
+    pub sides: HashMap<String, f64>,
 }
 
 impl Items {
@@ -20,11 +20,19 @@ impl Items {
     /// The function panics if the second argument is zero.
     ///
     /// ```rust
+    ///     use std::collections::HashMap;
+    ///     use map_macro::hash_map;
     ///     use dit_as_91896::menu::Items;
     ///
-    ///     let food: Vec<String> = vec![String::from("0")];
-    ///     let drinks: Vec<String> = vec![String::from("1")];
-    ///     let sides: Vec<String> = vec![String::from("2")];
+    ///     let food: HashMap<String, f64> = hash_map! {
+    ///         String::from("0") => 0.0,
+    ///     };
+    ///     let drinks: HashMap<String, f64> = hash_map! {
+    ///         String::from("1") => 1.0,
+    ///     };
+    ///     let sides: HashMap<String, f64> = hash_map! {
+    ///         String::from("2") => 2.0,
+    ///     };
     ///
     ///     let items: Items = Items::new(food.clone(), drinks.clone(), sides.clone());
     ///
@@ -32,7 +40,11 @@ impl Items {
     ///     assert!(items.drinks == drinks );
     ///     assert!(items.sides == sides);
     /// ```
-    pub fn new(food: Vec<String>, drinks: Vec<String>, sides: Vec<String>) -> Self {
+    pub fn new(
+        food: HashMap<String, f64>,
+        drinks: HashMap<String, f64>,
+        sides: HashMap<String, f64>,
+    ) -> Self {
         Self {
             food,
             drinks,
@@ -45,42 +57,49 @@ impl Display for Items {
     /// Fmt function allows the struct/type to be printed to the console
     ///
     /// ```rust
+    ///     use map_macro::hash_map;
     ///     use dit_as_91896::menu::Items;
     ///
     ///     let items: Items = Items {
-    ///         food: vec![String::from("0")],
-    ///         drinks: vec![String::from("1")],
-    ///         sides: vec![String::from("2")],
-    ///     };
+    ///     food: hash_map! {
+    ///         String::from("0") => 0.0,
+    ///     },
+    ///     drinks: hash_map! {
+    ///         String::from("1") => 1.0,
+    ///     },
+    ///     sides: hash_map! {
+    ///         String::from("2") => 2.0,
+    ///     },
+    /// };
     ///
-    ///     assert!(format!("{items}").as_str() == "Item: (food: [0], drinks: [1], sides: [2])");
+    ///     assert!(format!("{items}").as_str() == "food: {\n    0: costs $0.00\n},\ndrinks: {\n    1: costs $1.00\n},\nsides: {\n    2: costs $2.00\n},");
     /// ```
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let mut output: String = String::from("Item: (food: [");
+        let mut output: String = String::from("food: {\n");
 
-        if self.food.len() != 0usize {
-            for i in &self.food {
-                output.push_str(format!("{}, ", i.as_str()).as_str());
+        if self.food.keys().len() != 0usize {
+            for (item, price) in &self.food {
+                output.push_str(format!("    {}: costs ${:.2},\n", item.as_str(), price).as_str());
             }
-            output.replace_range((output.len() - 2).., "")
+            output.replace_range((output.len() - 2).., "\n")
         }
-        output.push_str("], drinks: [");
+        output.push_str("},\ndrinks: {\n");
 
-        if self.drinks.len() != 0usize {
-            for i in &self.drinks {
-                output.push_str(format!("{}, ", i.as_str()).as_str())
+        if self.drinks.keys().len() != 0usize {
+            for (item, price) in &self.drinks {
+                output.push_str(format!("    {}: costs ${:.2},\n", item.as_str(), price).as_str())
             }
-            output.replace_range((output.len() - 2).., "")
+            output.replace_range((output.len() - 2).., "\n")
         }
-        output.push_str("], sides: [");
+        output.push_str("},\nsides: {\n");
 
-        if self.drinks.len() != 0usize {
-            for i in &self.sides {
-                output.push_str(format!("{}, ", i.as_str()).as_str());
+        if self.drinks.keys().len() != 0usize {
+            for (item, price) in &self.sides {
+                output.push_str(format!("    {}: costs ${:.2},\n", item.as_str(), price).as_str());
             }
-            output.replace_range((output.len() - 2).., "")
+            output.replace_range((output.len() - 2).., "\n")
         }
-        output.push_str("])");
+        output.push_str("},");
 
         write!(f, "{output}")
     }
