2b5bfc8e94cb8777bdc68620d975fbd06edbc9ec
Liam Tietjens <l.tietjens@hotmail.com>
2024-04-02 18:57:04 +1300

    Started on a very basic cli interface extremly bugy but its only early

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,43 @@
-use map_macro::hash_map;
+use std::{
+    io::{stdout, Write},
+    process,
+};
 
-use dit_as_91896::food::Items;
+use crossterm::{
+    cursor::MoveUp,
+    execute,
+    terminal::{Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use ctrlc;
+use dit_as_91896::{
+    food::Items,
+    order::{Order, OrdersVec},
+};
+use map_macro::hash_map;
+use text_io::read;
+
+fn user_order(user_menu: &Items, cost_menu: &Items) -> Order {
+    let mut order = Order::new(user_menu.clone(), cost_menu.clone());
+    println!("MENU\n{}\n\n\nCURENT ORDER\n{order}", user_menu.menu_view());
+
+    loop {
+        print!("\nAdd To Order: ");
+        stdout().flush().unwrap();
+        let add: String = read!("{}\n");
+
+        order.order_add(add).unwrap();
+
+        print!("\n\n Do You Want To Add More [Y/N]");
+
+        let exit: String = read!("{}");
+
+        if exit.to_lowercase() == String::from("y") {
+            break;
+        }
+    }
+
+    order
+}
 
 fn main() {
     let food: Items = Items {
@@ -27,8 +64,33 @@ fn main() {
         },
     };
 
-    println!(
-        "to buy costs:\n{}\n\nto make costs:\n{}",
-        food, cost_of_food
+    // enters a seprate sestion kinda
+    println!("ENTERING SCREEN ...");
+    execute!(stdout(), EnterAlternateScreen, Clear(ClearType::All)).unwrap();
+
+    ctrlc::set_handler(move || {
+        execute!(stdout(), LeaveAlternateScreen).unwrap();
+        process::exit(1)
+    })
+    .unwrap();
+
+    let mut orders: Vec<Order> = Vec::new();
+
+    loop {
+        orders.push(user_order(&food, &cost_of_food));
+        break;
+    }
+
+    println!("{}", OrdersVec(orders.clone()));
+
+    // on exit it closes out
+    execute!(
+        stdout(),
+        LeaveAlternateScreen,
+        MoveUp(1),
+        Clear(ClearType::CurrentLine)
     )
+    .unwrap();
+
+    println!("{}", OrdersVec(orders));
 }
